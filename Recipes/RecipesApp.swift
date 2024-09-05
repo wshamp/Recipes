b13e62998254f699d561142d8ee24626d0873420
Wyeth Shamp <wyeth.shamp@gmail.com>
2024-09-04 22:53:16 -0400

    project creation

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Wyeth Shamp on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
